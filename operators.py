1cab55d9fbd9dbd037120f46e0f3f62081fc8310
Collinsniper <collinsniper01@gmail.com>
2020-02-06 10:03:09 +0300

    String Operators

---
+++ operators.py
@@ -0,0 +1,11 @@
+'''
+string operators
++,*,r,%,.format(),in,not in
+'''
+a="hello"
+b="world"
+print(a+b) #combine two strings
+print(a*4) #repeat the a string times of the number indicated
+print("hi {}".format(b))
+print('h' in a)
+print('n' not in a) #return True or False according to Boolean
